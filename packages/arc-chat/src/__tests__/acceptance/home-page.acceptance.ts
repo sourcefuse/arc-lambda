3d5edd3fef07fe6ed795651a22e48677a65aabc3
Sunny Tyagi <107617248+Tyagi-Sunny@users.noreply.github.com>
2023-08-23 12:04:15 +0530

    cdk added for notification service and chat service (#31)
    * cdk added for notification service and chat service
    
    * removed sonar issues
    
    * docker deploy in chat and notification service
    
    * include node_modules in dockerignore

---
+++ packages/arc-chat/src/__tests__/acceptance/home-page.acceptance.ts
@@ -0,0 +1,32 @@
+import {Client} from '@loopback/testlab';
+import {ChatApplication} from '../..';
+import {STATUS_CODE} from '../../enums/status-codes.enum';
+import {setupApplication} from './test-helper';
+
+describe('HomePage', () => {
+  let app: ChatApplication;
+  let client: Client;
+
+  before('setupApplication', async () => {
+    ({app, client} = await setupApplication());
+  });
+
+  after(async () => {
+    await app.stop();
+  });
+
+  it('exposes a default home page', async () => {
+    await client
+      .get('/')
+      .expect(STATUS_CODE.OK)
+      .expect('Content-Type', /text\/html/);
+  });
+
+  it('exposes self-hosted explorer', async () => {
+    await client
+      .get('/explorer/')
+      .expect(STATUS_CODE.OK)
+      .expect('Content-Type', /text\/html/)
+      .expect(/<title>LoopBack API Explorer/);
+  });
+});
---
+++ packages/arc-notification/src/__tests__/acceptance/home-page.acceptance.ts
@@ -0,0 +1,32 @@
+import {Client} from '@loopback/testlab';
+import {NotificationApplication} from '../..';
+import {STATUS_CODE} from '../../enums/status-codes.enum';
+import {setupApplication} from './test-helper';
+
+describe('HomePage', () => {
+  let app: NotificationApplication;
+  let client: Client;
+
+  before('setupApplication', async () => {
+    ({app, client} = await setupApplication());
+  });
+
+  after(async () => {
+    await app.stop();
+  });
+
+  it('exposes a default home page', async () => {
+    await client
+      .get('/')
+      .expect(STATUS_CODE.OK)
+      .expect('Content-Type', /text\/html/);
+  });
+
+  it('exposes self-hosted explorer', async () => {
+    await client
+      .get('/explorer/')
+      .expect(STATUS_CODE.OK)
+      .expect('Content-Type', /text\/html/)
+      .expect(/<title>LoopBack API Explorer/);
+  });
+});
