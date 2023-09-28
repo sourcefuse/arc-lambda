3aaf7b03c7f75fc5d907eeaee097ee7a5990b3e8
Vishwas Yogi <vishwas.yogi@sourcefuse.com>
2023-09-28 14:22:50 +0530

    feat(core): add arc-cdk-templates for sl cli

---
+++ arc-cdk-templates/lambda/lambda.ts
@@ -0,0 +1,29 @@
+import { APIGatewayEvent, APIGatewayProxyEvent, Context } from "aws-lambda";
+{{app_import_placeholder}}
+const serverlessExpress = require("@vendia/serverless-express");
+
+export * from "./application";
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let serverlessApp: (arg0: APIGatewayProxyEvent, arg1: Context) => any; // NOSONAR
+
+export async function setup(event: APIGatewayEvent, context: Context) {
+  const config = {
+    rest: {
+      openApiSpec: {
+        setServersFromRequest: true,
+      },
+    },
+  };
+  const app = new {{app_class_name_placeholder}}(config);
+  await app.boot();
+  const requestHandler = app.restServer.requestHandler;
+  serverlessApp = serverlessExpress({ app: requestHandler });
+  return serverlessApp(event, context);
+}
+
+export const handler = (event: APIGatewayEvent, context: Context) => {
+  if (serverlessApp) {
+    return serverlessApp(event, context);
+  }
+  return setup(event, context);
+};
